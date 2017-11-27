9195a594d45c5f83e473addfdf2e1f4d0762f899
Jérémie Sellam <jsellam@big-boss-studio.com>
2017-11-27 15:17:48 +0100

    update title

--- src/components/Top.vue
+++ src/components/Top.vue
@@ -2,7 +2,7 @@
   <div class="top">
     <a href="https://twitter.com/chloridrik" target="_blank" class="twitter-account">
         <span class="profile-picture"></span>
-        <span>@chloridrik</span>
+        <span>@chloridrik // portfolio</span>
     </a>
     <router-link :to="{path:'/'}"  v-if="currentTag != null" class="current-tag">#{{currentTag}}<i class="icon-cancel-circled-outline"></i></router-link>
   </div>
